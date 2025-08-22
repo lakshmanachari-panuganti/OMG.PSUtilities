1856a83981ebbd6870e98ce6e04e2d6209cb43a5
Lakshmanachari Panuganti <lucky.panuganti@rsmus.com>
2025-08-23 00:31:07 +0530

    feat: enhance module publishing and versioning logic! - Added logic to increment module versions and update changelogs automatically. - Improved error handling and logging during module publishing. - Updated module publishing workflow to use `Publish-Module -Name` instead of `-Path` and enhanced error message.

--- Module Developer Tools/Steps.ps1
+++ Module Developer Tools/Steps.ps1
@@ -7,20 +7,21 @@
     '$env:API_KEY_PERPLEXITY'
     '$env:API_KEY_PSGALLERY'
 ).foreach({
-    $envVarName = $_
-    $envVarValue = Invoke-Expression $envVarName
+        $envVarName = $_
+        $envVarValue = Invoke-Expression $envVarName
 
-    if([string]::IsNullOrEmpty($envVarValue) -or [string]::IsNullOrWhiteSpace($envVarValue)){
-        $readHost = $null
-        $readHost = Read-Host "The variable $envVarName is not set or empty. Please enter a value or press Enter to skip."
+        if ([string]::IsNullOrEmpty($envVarValue) -or [string]::IsNullOrWhiteSpace($envVarValue)) {
+            $readHost = $null
+            $readHost = Read-Host "The variable $envVarName is not set or empty. Please enter a value or press Enter to skip."
         
-        If(-not [string]::IsNullOrEmpty($readHost) -or -not [string]::IsNullOrWhiteSpace($readHost)){
-            Set-PSUUserEnvironmentVariable -Name $($envVarName.replace('$env:','')) -Value $readHost
+            If (-not [string]::IsNullOrEmpty($readHost) -or -not [string]::IsNullOrWhiteSpace($readHost)) {
+                Set-PSUUserEnvironmentVariable -Name $($envVarName.replace('$env:', '')) -Value $readHost
+            }
         }
-    } else{
-        Write-Host "The variable $envVarName is already set! skipping..." -ForegroundColor Green
-    }
-})
+        else {
+            Write-Host "The variable $envVarName is already set! skipping..." -ForegroundColor Green
+        }
+    })
 
 # Initiate
 . $($env:BASE_MODULE_PATH + '\Module Developer Tools\Set-ModuleEnvironmentVariables.ps1')
@@ -30,13 +31,14 @@ Write-Host "Importing Module Developer Tools functions..." -ForegroundColor Gree
 Get-ChildItem -Path ($env:BASE_MODULE_PATH + '\Module Developer Tools\functions') -Filter *.ps1 | ForEach-Object {
     try {
         . $_.FullName
-    } catch {
+    }
+    catch {
         Write-Error "Failed to load $($_.FullName): $_"
     }
 }
 
 # Test-PSUCommentBasedHelp
-Test-PSUCommentBasedHelp -ModulePath $env:BASE_MODULE_PATH | Where-Object{$_.HasHelpBlock -eq $false} | ForEach-Object {
+Test-PSUCommentBasedHelp -ModulePath $env:BASE_MODULE_PATH | Where-Object { $_.HasHelpBlock -eq $false } | ForEach-Object {
     Write-Host "Missing help block in: $($_.File)" -ForegroundColor Red
     Write-Host "Missing tags: $($_.MissingTags)" -ForegroundColor Yellow
     Write-Host ""
@@ -47,20 +49,43 @@ Get-OMGModules | Build-OMGModuleLocally -SkipScriptAnalyzer
 # NOTE: Increment of the module version is required when it is publishing to PSGallery!
 
 function omgpublishmodule {
-    Get-OMGModules | ForEach-Object {
-        try{
-            Publish-Module -Path $_.ModuleName -NuGetApiKey $env:apikey -Verbose
-        } catch {
-            Write-Warning "Failed to publish module $($_.ModuleName): $_"
+    $ModulesUpdated = Get-PSUGitFileChangeMetadata | 
+    Where-Object { 
+        $_.file -like 'OMG.PSUtilities.*/*/*.ps1' -and
+        $_.file -notlike 'OMG.PSUtilities.*/*/*--wip.ps1'  
+    } |
+    ForEach-Object { $_.file.split('/')[0] } | Sort-Object -Unique
+
+    $ModulesUpdated | Update-OMGModuleVersion -Increment Patch
+    git commit -m "Bump module versions "
+    $ModulesUpdated | Update-PSUChangeLog -ErrorAction Continue
+    $ModulesUpdated | Reset-OMGModuleManifests
+    $publishModule = Read-Host "Do you want to publish the updated modules to PSGallery? (Y/N)"
+    if ($publishModule -eq 'Y') {
+        $ModulesUpdated | ForEach-Object { 
+            $module = "$env:BASE_MODULE_PATH\$_"
+            try{
+                Publish-Module -Name $module -NuGetApiKey $env:apikey -Verbose -ErrorAction Stop
+                Write-Host "$module : Published successfully." -ForegroundColor Green
+            } Catch{
+                $Exception = $_.Exception.Message
+                if($Exception -like "* current version * is already available in the repository *"){
+                    Write-Host "$module : The current version is already available in the repository." -ForegroundColor Green
+                }
+                else {
+                    Write-Error "Failed to publish $module : $Exception"
+                }
+            }
         }
     }
 }
 
 function omgupdatemodule {
     Get-OMGModules | ForEach-Object {
-        try{
+        try {
             Update-Module -Name $_.ModuleName -Verbose -Force
-        } catch {
+        }
+        catch {
             Write-Warning "Failed to update module $($_.ModuleName): $_"
         }
     }
@@ -68,9 +93,10 @@ function omgupdatemodule {
 
 function omgbuildmodule {
     Get-OMGModules | ForEach-Object {
-        try{
+        try {
             Build-OMGModuleLocally -ModuleName $_.ModuleName -SkipScriptAnalyzer -Verbose
-        } catch {
+        }
+        catch {
             Write-Warning "Failed to build module $($_.ModuleName): $_"
         }
     }
